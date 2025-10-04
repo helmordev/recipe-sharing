78b71fd25a4b457ff207bd96a490e03ee26d7478
Poging Programmer <helmorpogi@gmail.com>
2025-10-04 23:01:46 +0800

    feat: add policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Recipe;
+use App\Models\Comment;
+use App\Policies\RecipePolicy;
+use App\Policies\CommentPolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Comment::class => CommentPolicy::class,
+        Recipe::class => RecipePolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
@@ -19,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
     }
 }
